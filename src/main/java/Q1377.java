bf09e62d8ffb261eefa2a491e4ca53dd1742665a
YerinCho <forjustice9839@gmail.com>
2021-05-19 19:46:18 +0900

    2주차 정렬

---
+++ src/main/java/Q1377.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Q1377 {
+    private static int N;
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> inputs = new ArrayList<>();
+        initialize(inputs);
+        int result = 0;
+        List<Integer> sorted = inputs.stream()
+            .sorted()
+            .collect(Collectors.toList());
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            counts.put(sorted.get(i), i);
+        }
+        for (int i = 0; i < N; i++) {
+            int n = counts.get(inputs.get(i));
+            int difference = i - n;
+            result = Math.max(result, difference);
+        }
+
+        bw.write(result + 1);
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void initialize(List<Integer> inputs) throws IOException {
+        String[] firstLine = br.readLine().split(" ");
+        N = Integer.parseInt(firstLine[0]);
+
+        for (int i = 0; i < N; i++) {
+            inputs.add(Integer.parseInt(br.readLine()));
+        }
+    }
+}
---
+++ src/main/java/Q18870.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class Q18870 {
+    private static int N;
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> inputs = new ArrayList<>();
+        initialize(inputs);
+        List<Integer> deduplicated = new ArrayList<>(new HashSet<>(inputs));
+
+        deduplicated.sort(Comparator.naturalOrder());
+
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i = 0; i < deduplicated.size(); i++) {
+            result.put(deduplicated.get(i), i);
+        }
+
+        for (Integer input : inputs) {
+            bw.write(result.get(input) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void initialize(List<Integer> inputs) throws IOException {
+        String[] firstLine = br.readLine().split(" ");
+        N = Integer.parseInt(firstLine[0]);
+
+        String[] secondLine = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            inputs.add(Integer.parseInt(secondLine[i]));
+        }
+    }
+}
